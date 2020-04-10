b7cdf6db8e9ef1d65da6213793a688dcd6ec9714
RyanJE <RyanJE@users.noreply.github.com>
2020-04-11 03:42:43 +1000

    Got level1 running

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from flask import Flask, request, render_template
+from threading import Lock
+import secrets
 app = Flask(__name__)
+
+class TokenCache():
+    def __init__(self):
+        self.cache = {}
+        self.lock = Lock()
+    def addToCache(self, key):
+        self.lock.acquire()
+        if (key not in self.cache):
+            self.cache[key] = secrets.token_urlsafe()
+            self.lock.release()
+            return self.cache[key]
+        else:
+            self.lock.release()
+            raise Exception
+    def queryCache(self, key):
+        return self.cache[key]
+    def removeFromCache(self, key):
+        self.lock.acquire()
+        del self.cache[key]
+        self.lock.release()
+
+tokenCache2 = TokenCache()
+tokenCache3 = TokenCache()
 
 @app.route("/")
 def home():
@@ -9,8 +34,13 @@ def home():
 def level1():
     return render_template("level1/level1.html")
 
-@app.route("/level1/querydb")
+@app.route("/level1/querydb", methods=['GET'])
 def l1querydb():
+    try:
+        if (tokenCache1.queryCache(request.form['name']) != request.form['token']):
+            raise Exception
+    except:
+        return "Unauthorised user"
     pass
 
 @app.route("/level2")
--- start.py
+++ start.py
@@ -1,5 +1,28 @@
 import os
-
+import sqlite3
+from encoding import vigEncode
 os.environ["FLASK_APP"] = "app.py"
+
+
+#DONT LOOK AT THIS FUNCTION YOU CHEATER, IT'LL RUIN EVERYTHING
+def startup():
+    conn = sqlite3.connect('./templates/level1/data.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE users")
+    c.execute("CREATE TABLE users (username text, secret text)")
+    for (user, secret) in [("admin",vigEncode("SecretIsInUser554", "SecretIsInUser554")), 
+    ("admin",vigEncode("FLAG[LEVEL_1_COMPLETED_YOU_ARE_COOL]", "FLAG[LEVEL_1_COMPLETED_YOU_ARE_COOL]")), 
+    ("admin",vigEncode("NotThisOne", "NotThisOne")), ("admin",vigEncode("ThisOneNotHere", "ThisOneNotHere"))]:
+        c.execute("INSERT INTO users VALUES ('{}','{}')".format(user, secret))
+    conn.commit()
+
+    conn.close()
+    conn = sqlite3.connect('./templates/level2/data.db')
+    conn.close()
+    conn = sqlite3.connect('./templates/level3/data.db')
+    conn.close()
+
+
+startup()
 
 os.system("flask run")
