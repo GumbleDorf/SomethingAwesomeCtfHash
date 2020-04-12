c1c41c0014c5a35bb997bf1de409135dac8f42ce
RyanJE <RyanJE@users.noreply.github.com>
2020-04-13 01:52:17 +1000

    finished

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, render_template
 from threading import Lock
+from start import level1Pass, level3Pass
+from encoding import caesar, randomNum
 import secrets
+import sqlite3
+import json
+import string
+import os
+import random
 app = Flask(__name__)
 
 class TokenCache():
@@ -23,8 +30,7 @@ class TokenCache():
         del self.cache[key]
         self.lock.release()
 
-tokenCache2 = TokenCache()
-tokenCache3 = TokenCache()
+tokenCache = TokenCache()
 
 @app.route("/")
 def home():
@@ -34,19 +40,148 @@ def home():
 def level1():
     return render_template("level1/level1.html")
 
-@app.route("/level1/querydb", methods=['GET'])
-def l1querydb():
+@app.route("/level1/verify", methods=['GET'])
+def l1verify():
     try:
-        if (tokenCache1.queryCache(request.form['name']) != request.form['token']):
-            raise Exception
-    except:
-        return "Unauthorised user"
-    pass
+        alphabets = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+        code = request.args.get('code')
+        conn = sqlite3.connect('./templates/level1/data.db').cursor()
+        encodedcode = caesar(code, len(code), alphabets)
+        statement = "SELECT * FROM users WHERE secret='{}'".format(encodedcode)
+        conn.execute(statement)
+        results = conn.fetchall()
+        conn.close()
+        #THIS IS PURPOSELY REALLY BAD CODE
+        match = False
+        for (a, b) in results:
+            if (a == "User554" and b == code):
+                match = True
+        return {
+            "match": match,
+            "debugData": statement + "|"+ str(results)
+        }
+    except Exception as e:
+        return {
+            "match": False,
+            "debugData": statement + "|"+str(e)
+        }
+
+
+def evalString(message):
+    if message == None:
+        return message
+    else:
+        newMessage = ""
+        subMessage = ""
+        evalOn = False
+        for i in message:
+            if i == '{' and not evalOn:
+                evalOn = True
+            elif i != '}' and evalOn:
+                subMessage += i
+            elif i == '}' and evalOn:
+                evalOn = False
+                print(eval(subMessage))
+                try:
+                    tmpMes = eval(subMessage)
+                    
+                    newMessage += str(tmpMes)
+                except:
+                    newMessage += subMessage
+                subMessage = ""
+            else:
+                newMessage += i
+    return newMessage
+
+
+
+@app.route("/level2/makepost", methods=['GET'])
+def l2post():
+    post = evalString(request.args.get('post'))
+    conn = sqlite3.connect('./templates/level2/data.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO posts VALUES (?,?)', [request.args.get('user'), post])
+    conn.commit()
+    conn.close()
+    return {
+        "success": True,
+        "post": post
+    }
+
+@app.route("/level2/verify", methods=['GET'])
+def l2verify():
+    conn = sqlite3.connect('./templates/level2/data.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    res = c.fetchall()
+    conn.close()
+    for (user, password) in res:
+        if(request.args.get('user') == user):
+            if (request.args.get('pass') == password):
+                return {
+                    "match": True,
+                    "passwordMatch": True
+                }
+            else:
+                return {
+                    "match": True,
+                    "passwordMatch": False
+                }
+    return {
+        "match": False,
+        "passwordMatch": False
+    }
 
 @app.route("/level2")
 def level2():
-    return "not implemented"
+    return render_template("level2/level2.html")
+
+@app.route("/level2/posts")
+def level2posts():
+    conn = sqlite3.connect('./templates/level2/data.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM posts WHERE user=?', [request.args.get('user')])
+    res = c.fetchall()
+    conn.close()
+    return {
+        "posts": [b for (a,b) in res]
+    }
+
+
+seed=random.randint(0,127)
+def nextNum():
+    global seed
+    seed=(seed + 1)%128
+    return seed
+def encrypt(message):
+    stringNums = string.ascii_letters + string.digits
+    encryptedMessage = ""
+    for i in message:
+        nextNum()
+        encryptedMessage += stringNums[(ord(i) + randomNum(seed))%len(stringNums)]
+    return encryptedMessage
+
+
+@app.route("/level3/encrypt", methods=['GET'])
+def level3encrypt():
+    username=request.args.get('user')
+    password=request.args.get('pass')
+    message=request.args.get('message')
+    conn = sqlite3.connect('./templates/level3/data.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE username='"+username+"' AND password='"+password+"'")
+    res = c.fetchall()
+    if(len(res) != 1):
+        return {
+            "auth": False,
+            "encryptedMessage": ""
+        }
+    else:
+        return {
+            "auth": True,
+            "encryptedMessage": encrypt(message)
+        }
 
 @app.route("/level3")
 def level3():
-    return "not implemented"
+    return render_template("level3/level3.html", encrypted=encrypt(level3Pass))
--- start.py
+++ start.py
@@ -1,28 +1,68 @@
 import os
 import sqlite3
-from encoding import vigEncode
+from encoding import caesar
+import string
+import random
+
 os.environ["FLASK_APP"] = "app.py"
 
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
-#DONT LOOK AT THIS FUNCTION YOU CHEATER, IT'LL RUIN EVERYTHING
+level1Pass = "FLAG"+randomString(20)
+level2Pass = "FLAG"+randomString(20)
+level3Pass = "FLAG"+randomString(20)
+
+#DONT LOOK AT THIS FUNCTION YOU CHEATER, IT'LL RUIN EVERYTHING (unless you are on part 2, in which case its fair game)
 def startup():
     conn = sqlite3.connect('./templates/level1/data.db')
     c = conn.cursor()
-    c.execute("DROP TABLE users")
+    c.execute("DROP TABLE IF EXISTS users")
     c.execute("CREATE TABLE users (username text, secret text)")
-    for (user, secret) in [("admin",vigEncode("SecretIsInUser554", "SecretIsInUser554")), 
-    ("admin",vigEncode("FLAG[LEVEL_1_COMPLETED_YOU_ARE_COOL]", "FLAG[LEVEL_1_COMPLETED_YOU_ARE_COOL]")), 
-    ("admin",vigEncode("NotThisOne", "NotThisOne")), ("admin",vigEncode("ThisOneNotHere", "ThisOneNotHere"))]:
+    for (user, secret) in level1UserList():
         c.execute("INSERT INTO users VALUES ('{}','{}')".format(user, secret))
     conn.commit()
-
     conn.close()
+    
     conn = sqlite3.connect('./templates/level2/data.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS users")
+    c.execute("CREATE TABLE users (username text, password text)")
+    c.execute("DROP TABLE IF EXISTS posts")
+    c.execute("CREATE TABLE posts (user text, post text)")
+    for (user, password) in level2UserList():
+        c.execute("INSERT INTO users VALUES ('{}','{}')".format(user, password))
+    conn.commit()
     conn.close()
+    f = open('./templates/level2/secretlvl2password.txt', "w")
+    f.write(level2Pass)
+    f.close()
+
     conn = sqlite3.connect('./templates/level3/data.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS users")
+    c.execute("CREATE TABLE users (username text, password text)")
+    for (user, password) in level2UserList():
+        c.execute("INSERT INTO users VALUES ('{}','{}')".format(user, password))
+    conn.commit()
     conn.close()
 
 
-startup()
 
-os.system("flask run")
+
+def level1UserList():
+    alphabets = (string.ascii_lowercase, string.ascii_uppercase, string.digits)
+    return [("admin",caesar("SecretIsInUser554", len("SecretIsInUser554"), alphabets)), 
+    ("User554",caesar(level1Pass, len(level1Pass), alphabets)), 
+    ("User6000",caesar("NotThisOne", len("NotThisOne"), alphabets)), ("User666",caesar("ThisOneNotHere", len("ThisOneNotHere"), alphabets)),
+    ("User555",caesar("abcdefghijklmnopqrstuvwxyz", len("abcdefghijklmnopqrstuvwxyz"), alphabets))]
+
+def level2UserList():
+    return [("admin", randomString(100))] + [(i, randomString(100)) for i in string.ascii_letters]
+
+
+if __name__ == "__main__":
+    startup()
+    os.system("flask run")
